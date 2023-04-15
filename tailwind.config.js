aa3851dc001d7064d2a55a8c02684242e6d9ef50
zeiadsalhin <zeyadali011@gmail.com>
2023-04-15 21:35:29 +0200

    v2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/**/*.{html,js}"],
+  content: ["./**/**/*.{html,js}"],
   darkMode: "class",
   theme: {
     extend: {},
