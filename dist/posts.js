9e97ccae21b29241b1f69ce598778f5dae5819a5
zeiadsalhin <zeyadali011@gmail.com>
2023-04-27 21:37:55 +0200

    v2

--- dist/posts.js
+++ dist/posts.js
@@ -1,31 +1,31 @@
-// write
+// // write
 
-const post = document.getElementById("par");
+// const post = document.getElementById("par");
 
-// date of publish
-let date = "2020";
+// // date of publish
+// let date = "2020";
 
-// title
-let title = "t";
+// // title
+// let title = "t";
 
-// article
-let article = "t";
+// // article
+// let article = "t";
 
-//link
-let link = "";
+// //link
+// let link = "";
 
-//content
-let content = `
+// //content
+// let content = `
 
-<div class="flex-col mr-5 ml-5 mb-10">
-<div class="w-80 c4-izmir c4-image-zoom-in"><img src="./1.jpg" width="300" height="300" alt=""></div>
-<p class="text-xs text-gray-600 dark:text-gray-300">Published at - ${date} </p>
-<p class="text-2xl">${title}</p>
-<hr class="m-2 border-gray-400 dark:border-gray-200">
-<p>${article}</p>
-<a href="${link}" class="text-xs flex justify-end">Read more...</a>
-</div>
+// <div class="flex-col mr-5 ml-5 mb-10">
+// <div class="w-80 c4-izmir c4-image-zoom-in"><img src="./1.jpg" width="300" height="300" alt=""></div>
+// <p class="text-xs text-gray-600 dark:text-gray-300">Published at - ${date} </p>
+// <p class="text-2xl">${title}</p>
+// <hr class="m-2 border-gray-400 dark:border-gray-200">
+// <p>${article}</p>
+// <a href="${link}" class="text-xs flex justify-end">Read more...</a>
+// </div>
 
-          `;
+//           `;
 
-post.insertAdjacentHTML("afterend", content);
+// post.insertAdjacentHTML("afterend", content);
