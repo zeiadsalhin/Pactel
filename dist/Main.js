cb108b27b8d22e822eb7d55846eabc6d5d9b078d
zeiadsalhin <zeyadali011@gmail.com>
2023-08-27 06:32:48 +0300

    v1

--- dist/Main.js
+++ dist/Main.js
@@ -99,3 +99,27 @@ document.addEventListener("click", function () {
   }),
   (date = new Date()),
   (document.getElementById("copyrights").innerText = date.getFullYear());
+
+  let nums = document.querySelectorAll(".nums .num");
+let section = document.querySelector(".three");
+let started = false;
+
+window.onscroll = function () {
+  if (window.scrollY >= section.offsetTop) {
+    if (!started) {
+      nums.forEach((nums) => startcount(nums));
+    }
+    started = true;
+  }
+};
+
+function startcount(el) {
+  let goal = el.dataset.goal;
+  let count = setInterval(() => {
+    el.textContent++;
+
+    if (el.textContent == goal) {
+      clearInterval(count);
+    }
+  }, 500 / goal);
+}
