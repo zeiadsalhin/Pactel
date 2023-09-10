219cbfaffb6fddcf34703b31bd0726bd9ed2301f
zeiadsalhin <zeyadali011@gmail.com>
2023-09-10 05:35:02 +0300

    v1

--- dist/Main.js
+++ dist/Main.js
@@ -75,7 +75,7 @@ function serv() {
   if (window.innerWidth < 768) {
     document.querySelector("#serv").scrollIntoView({ behavior: 'smooth' });
     let position = document.querySelector("#serv").getBoundingClientRect();
-    window.scrollTo(position.left, position.top + window.scrollY - 400);
+    window.scrollTo(position.left, position.top + window.scrollY - 700);
     console.log("done")
   } else if (window.innerWidth > 768) {
     document.querySelector("#serv").scrollIntoView({ behavior: 'smooth' });
