71ac4c43924a0e94201d819ecb54a35105940eeb
zeiadsalhin <zeyadali011@gmail.com>
2023-10-03 02:06:53 +0300

    v1

--- src/views/Quiz.vue
+++ src/views/Quiz.vue
@@ -5,8 +5,8 @@
     <div class="content md:p-10 flex-col justify-center mx-auto">
         <img class="p-2 mx-auto dark:invert w-auto md:w-11/12" src="/pactel_logo.webp" width="100%" height="100%"
             alt="pactel">
-        <iframe class="mx-auto w-full dark:invert" scrolling="no" allowfullscreen title="quiz"
+        <iframe class="mx-auto w-full dark:invert absolute" allowfullscreen title="quiz"
             src="https://docs.google.com/forms/d/e/1FAIpQLSe546I-JsIXezcx77T-iCJoZIW6eNMlxikgeLxsOg-1nzBfOg/viewform?embedded=true"
-            width="640" height="2450" frameborder="0" marginheight="0" marginwidth="0">جارٍ التحميل…</iframe>
+            width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0">جارٍ التحميل…</iframe>
     </div>
 </template>
