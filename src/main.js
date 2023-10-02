b9de58dd6949bdc559ca945d6c4c5b125bb3cbf3
zeiadsalhin <zeyadali011@gmail.com>
2023-10-02 22:18:37 +0300

    v1

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import './assets/main.css'
 import './assets/index.css'
-
+import "../public/vanilla-js-pages-transition-loader.min.js"
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
