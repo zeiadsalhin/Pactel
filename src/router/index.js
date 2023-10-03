46dcf189a8c66c7c65affcaaa27990ae41b056f5
zeiadsalhin <zeyadali011@gmail.com>
2023-10-03 11:27:47 +0300

    v1

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,6 @@ import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  mode: "hash",
   routes: [
     {
       path: '/',
