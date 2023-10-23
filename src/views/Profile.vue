fe0bcc6554d131bba185a6e52801dfa340836797
zeiadsalhin <zeyadali011@gmail.com>
2023-10-23 04:24:13 +0300

    v1

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -163,7 +163,7 @@ function deleteuser() {
         <h1 class="text-2xl md:text-4xl dark:text-white text-center font-bold p-2">ادارة الحساب</h1>
         <div class="w-1/4 h-1 mt-5 rounded-xl mx-auto bg-gray-600 dark:bg-gray-900"></div>
         <div class="bg-gray-200 dark:bg-gray-800 mt-5 w-1/2 mx-auto">
-            <div id="loader" class="bg-gray-200 dark:bg-gray-800 w-fit animate-pulse mx-auto"><img id="profile"
+            <div id="loader" class="bg-gray-300 dark:bg-gray-800 w-96 animate-pulse mx-auto"><img id="profile"
                     @load="imageload" src="/logo_test.svg" class="mx-auto p-4 opacity-0" width="250" height="200"
                     alt="user">
             </div>
